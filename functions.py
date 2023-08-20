4b41ab407556be6b49ccf57b582c6c3bb4273a2a
Arman Rizal <inazrabuu@gmail.com>
2023-08-20 14:48:04 +0700

    add another function to functions.py

--- functions.py
+++ functions.py
@@ -7,6 +7,15 @@ def get_completion(openai, prompt, model="gpt-3.5-turbo"):
   )
   return response.choices[0].message["content"]
 
+def temperature_completion(openai, prompt, model="gpt-3.5-turbo", temperature=0):
+  messages = [{"role": "user", "content": prompt}]
+  response = openai.ChatCompletion.create(
+      model=model,
+      messages=messages,
+      temperature=temperature, 
+  )
+  return response.choices[0].message["content"]
+
 def o(message):
   return f"""
     hello {message}
