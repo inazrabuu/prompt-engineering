ba1f370f9b765c24c9bf7ed61e483dfb38ce086d
Arman Rizal <inazrabuu@gmail.com>
2023-08-20 19:18:07 +0700

    add a function to functions

--- functions.py
+++ functions.py
@@ -16,6 +16,15 @@ def temperature_completion(openai, prompt, model="gpt-3.5-turbo", temperature=0)
   )
   return response.choices[0].message["content"]
 
+def get_completion_from_messages(openai, messages, model="gpt-3.5-turbo", temperature=0):
+  response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages,
+    temperature=temperature, # this is the degree of randomness of the model's output
+  )
+
+  return response.choices[0].message["content"]
+
 def o(message):
   return f"""
     hello {message}
